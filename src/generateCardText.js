edfd16d4895c04396e7dded18cd78c7724802f04
KAkos27 <akoskosztolanyi@gmail.com>
2024-06-06 15:11:13 +0200

    reszponzivitas javitasa

--- src/generateCardText.js
+++ src/generateCardText.js
@@ -4,8 +4,8 @@ const generateCardText = (toys) => {
     text += `
       <div class='card'>
         <h3><strong>${toy.name}</strong></h3>
-        <p class="card-info">${toy.info}</p>
-        <p class="card-price"><strong>${toy.price} Ft</strong></p>
+        <p class="card__info">${toy.info}</p>
+        <p class="card__price"><strong>${toy.price} Ft</strong></p>
         <button class='card__add-button' id=${toy.id}>Kosárba</button>
         <img src='/toy-pic.svg' alt=''>
       </div>`;
